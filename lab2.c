d170366e251eb1d41ea09be409e579743e09f383
Victoria <teodora.victoria28@gmail.com>
2024-03-06 18:45:22 +0200

    abc

---
+++ lab2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int function(int x){
+	if(x<=0)
+        return x;
+    else{
+	x=x-2;
+	x=function(x);
+	return x;
+    }
+}
+
+int main(){
+	int a=30, i=0;
+
+	while (a>0){
+		a=function(a);
+		i++;
+	}
+	printf("%d", i);
+	return 0;
+}
+
